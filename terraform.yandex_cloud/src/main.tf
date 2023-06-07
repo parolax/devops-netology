9de99d8f33edf8e0fc578118520959031f85d734
Sergey Vilkov <7921920@gmail.com>
2023-06-07 20:42:57 +0300

    Added src files up to homework 4

---
+++ terraform.yandex_cloud/src/main.tf
@@ -0,0 +1,70 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+
+data "yandex_compute_image" "ubuntu" {
+  family = var.vm_web_ubuntu
+}
+
+resource "yandex_compute_instance" "platform" {
+  name        = var.vm_web_platform
+  platform_id = var.vm_web_platform_id
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+
+}
+
+resource "yandex_compute_instance" "platform-db" {
+  name        = var.vm_db_platform
+  platform_id = var.vm_db_platform_id
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+
+}
---
+++ terraform.yandex_cloud/src/variables.tf
@@ -0,0 +1,41 @@
+###cloud vars
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDiiO0rw5KT/eChfFXbEKGS58XmFHijmkMPqIJUou/09M+oiORS5Ar/s+tRHOodlUa3LAwjuTWh3HdmjSFz8heP4e4rDE8iMt3Etkwzs0GGu9PCWNmQ/un4tFXyQza13QT3SA1V4dYPYq4I8cLrjymU5MW3KABv1xwtuQeo1FQztaqZDqPTKNWs8y4aHmi1mq2nHjSJLbg1Bo47lrLj1/cF2y8Qghhcw6NrYQEdPST37tb+YhEEyRHivrIzWVAMJWjfMqi2JDrbA3cBGqJJgjBPBP3x0sbutOoHYdQrJFrjgzESgNbmt121a12XSTr0Mtbo0d+f3WEIFOmVEy5yx0OtTaj4p7HoSAa6jrpKDFxMkMyKJWcn8aKp2EIgA/DgrSu5RHHmmL5lJOko0jw3kxzeuVP4P/nJzy8Ah1GvD/yUQq8swGqS41LpuDbqk6VPeFra9HwSNT2591EbU1Hf1eVLeWlcm8PahHdLREgCqXC+7v+K5UlGxH6UxK8JU2XbTaM= sergeo@Sergeys-MacBook-Air.local"
+  description = "ssh-keygen -t ed25519"
+}
